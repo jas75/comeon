0aebb61dd9d4a62a4b79863164290e83eb05372f
Jason <azoulayjason@gmail.com>
2019-07-17 03:27:28 +0200

    Implement RTCMulticonnection

--- public/chat.js
+++ public/chat.js
@@ -1,6 +1,6 @@
 // Make a socket.io connection
 var socket = io();
-
+ 
 // Assign variables to query the DOM
 var message = document.getElementById('message'),
       handle = document.getElementById('handle'),
@@ -30,3 +30,25 @@ socket.on('chat', function(data){
 socket.on('typing', function(data){
     feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
 });
+
+var connection = new RTCMultiConnection();
+
+//connection.socketUrl = "http://localhost:3000";
+//connection.socketUrl = socket.io.uri;
+ connection.socketURL = 'https://rtcmulticonnection.herokuapp.com:443/';
+connection.session = {
+    audio: true,
+    video: true
+};
+connection.sdpConstraints.mandatory = {
+    OfferToReceiveAudio: true,
+    OfferToReceiveVideo: true
+};
+
+
+document.getElementById('btn-open-or-join-room').onclick = function() {
+    this.disabled = true;
+
+    connection.openOrJoin('predefiend-roomid')
+
+}
